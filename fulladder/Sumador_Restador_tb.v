6cbbc827db7ea8e82ff2020405b1c959a3a446d0
DanielCastro-02 <64162547+DanielCastro-02@users.noreply.github.com>
2023-10-10 23:42:39 -0500

    Update Sumador_Restador_tb.v

--- fulladder/Sumador_Restador_tb.v
+++ fulladder/Sumador_Restador_tb.v
@@ -1,16 +1,5 @@
 module testbech;
 
-  // STIMULUS
-  // reg a = 0, b = 0;
-  // initial
-  // begin
-  //   # 17 a = 1, b = 1;
-  //   # 11 a = 1, b = 0;
-  //   # 29 a = 1, b = 1;
-  //   # 11 a = 1, b = 0;
-  //   # 100 $finish(); // [stop(), $finish()]
-  // end
-
   reg [8:0] inputs;
   /* inputs[0] inputs[1] inputs[2] */
   integer i;
@@ -23,13 +12,7 @@ module testbech;
     end
   end
 
-  // CLOCK STIMULUS
-  // Make a regular pulsing clock.
-  // reg clk = 0;
-  // always #5 clk = !clk;
-
-  /* // RESULT FOR DEVICE/DESIGN UNDER TEST */
-  wire [4:0] outs;
+   wire [4:0] outs;
   // DEVICE/DESIGN UNDER TEST
   Sumador_Restador dut (
      .inb_3(inputs[8]), .inb_2(inputs[7]), .inb_1(inputs[6]), .inb_0(inputs[5]), .ina_3(inputs[4]), .ina_2(inputs[3]), .ina_1(inputs[2]), .ina_0(inputs[1]), .inci_0(inputs[0]), 
@@ -37,13 +20,6 @@ module testbech;
   
   
   );
-
-  /* // MONITOR */
-  // initial
-  //   $monitor("Time: %t, a = %d, b = %d => out = %d",
-  //     $time, a, b, value);
-
-  // WAVES IN VCD TO OPEN IN GTKWAVE
   initial
   begin
     $dumpfile("top.vcd");
