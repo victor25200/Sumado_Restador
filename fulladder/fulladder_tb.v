ee924e42a4953b25dd23709629e025ed33cd1b8d
DanielCastro-02 <64162547+DanielCastro-02@users.noreply.github.com>
2023-10-10 23:24:33 -0500

    Update fulladder_tb.v

--- fulladder/fulladder_tb.v
+++ fulladder/fulladder_tb.v
@@ -1,16 +1,5 @@
 module testbech;
 
-  // STIMULUS
-  // reg a = 0, b = 0;
-  // initial
-  // begin
-  //   # 17 a = 1, b = 1;
-  //   # 11 a = 1, b = 0;
-  //   # 29 a = 1, b = 1;
-  //   # 11 a = 1, b = 0;
-  //   # 100 $finish(); // [stop(), $finish()]
-  // end
-
   reg [2:0] inputs;
   /* inputs[0] inputs[1] inputs[2] */
   integer i;
@@ -22,12 +11,6 @@ module testbech;
       #1;
     end
   end
-
-  // CLOCK STIMULUS
-  // Make a regular pulsing clock.
-  // reg clk = 0;
-  // always #5 clk = !clk;
-
   /* // RESULT FOR DEVICE/DESIGN UNDER TEST */
   wire [1:0] outs;
   // DEVICE/DESIGN UNDER TEST
@@ -35,11 +18,6 @@ module testbech;
     .in_b(inputs[2]), .in_a(inputs[1]), .in_ci(inputs[0]),
     .out_co(outs[1]), .out_s(outs[0])
   );
-
-  /* // MONITOR */
-  // initial
-  //   $monitor("Time: %t, a = %d, b = %d => out = %d",
-  //     $time, a, b, value);
 
   // WAVES IN VCD TO OPEN IN GTKWAVE
   initial
